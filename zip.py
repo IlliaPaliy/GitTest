c0127610c355d78c36feada3c6bc4e93c77c88f1
Illia Paliy <cattano285@gmail.com>
2021-08-15 12:14:34 +0300

    changed zip.py

--- zip.py
+++ zip.py
@@ -1,5 +1,5 @@
 import zipfile
 
-jungle_zip = zipfile.ZipFile('D:\\Codes\\git\\gittest.zip','w')
-jungle_zip.write('GitTest\\run.py', compress_type=zipfile.ZIP_DEFLATED)
+jungle_zip = zipfile.ZipFile('gittest.zip','w')
+jungle_zip.write('D:\\Codes\\git\\run.py', compress_type=zipfile.ZIP_DEFLATED)
 jungle_zip.close()
